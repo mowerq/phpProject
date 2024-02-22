17a87ae2ba788cd0a7ef4aaaf08751cee93dcd3a
mowerq <murat.gurgenyatagi@gmail.com>
2024-02-22 12:58:12 +0300

    try new files

--- videoScrolling/script.js
+++ videoScrolling/script.js
@@ -1,15 +1,36 @@
-let player = document.getElementById("firstLottie");
+/*window.onload = function () {
+  const video = document.querySelector("video");
+  const playBackSpeed = 600;
+  document.body.style.height =
+    Math.floor(video.duration) * playBackSpeed + "px";
+  window.addEventListener("scroll", function () {
+    video.currentTime = window.scrollY / playBackSpeed;
+  });
+};
+const video = document.querySelector("video");
 
-player.addEventListener("ready", () => {
-  LottieInteractivity.create({
-    mode: "scroll",
-    player: "#firstLottie",
-    actions: [
-      {
-        visibility: [0, 1.0],
-        type: "seek",
-        frames: [0, 300],
-      },
-    ],
+function handleVideoVisible() {
+  observer.unobserve(vid);
+  observer.observe(vid);
+}
+
+let observer = new IntersectionObserver((entries, observer) => {
+  entries.forEach((entry) => {
+    if (window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
+      vid.currentTime = 1;
+    } else {
+      if (entry.isIntersecting) {
+        console.log("iceride");
+        window.addEventListener("scroll", handleVideoVisible);
+        if (entry.intersectionRatio > 0) {
+          video.currentTime = video.duration * entry.intersectionRatio;
+        }
+      } else {
+        console.log("Dısarıda");
+        window.removeEventListener("scroll", handleVideoVisible);
+      }
+    }
   });
 });
+
+observer.observe(vid);*/
